226696027ba425e7aee2c1cf677d31e3fd4c349e
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2018-07-11 10:57:02 -0400

    Initializing projet

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,11 @@
 import * as React from "react";
-// @ts-ignore
-import * as img from "../assets/start_coding.jpg";
 import "../styles/App.scss";
-import Hello from "./Hello";
 
 class App extends React.Component {
   public render() {
     return (
       <div>
-        <Hello compiler="TypeScript" framework="React" />
-        <p>
-          This react and typescript app is configured with Webpack 4 and Babel. It also features
-          File loader and Scss. You can start coding now !
-        </p>
-        <br/>
-        <img src={img} alt="Start coding !" />
+        <h1>Cine project</h1>
       </div>
     );
   }
